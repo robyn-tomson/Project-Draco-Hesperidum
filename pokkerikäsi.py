5d9fadff1179b69ad413c7c008d6d02bba6170f4
Robyn Tomson <robyntomson@gmail.com>
2023-10-30 23:04:07 +0200

    returni  muutmine pooleli atm

--- pokkerikäsi.py
+++ pokkerikäsi.py
@@ -1,4 +1,5 @@
 def pokerhand(p_kasi):
+    temp = []
     kasi = []
     l = 0
     n = [0, 2, 4, 6, 8]
@@ -12,54 +13,54 @@ def pokerhand(p_kasi):
         if l in n:
             if i == 'J':
                 number.append(11)
+                number.append('11')
             elif i == 'Q':
                 number.append(12)
+                number.append('12')
             elif i == 'K':
                 number.append(13)
+                number.append('13')
             elif i == 'A':
                 number.append(14)
+                number.append('14')
             else:
                 number.append(int(i))
+                number.append(i)
         l += 1
     number = sorted(number)
     if kasi.count('♥') == 5 or kasi.count('♦') == 5 or kasi.count('♣') == 5 or kasi.count('♠') == 5:
-        if kasi.count('10') == 1 and kasi.count('J') == 1 and kasi.count('Q') == 1 and kasi.count('K') == 1 and kasi.count('A') == 1:
-            return('Kuninglik mastirida')
-            quit()
+        if number.count('10') == 1 and number.count('11') == 1 and number.count('12') == 1 and number.count('13') == 1 and number.count('14') == 1:
+            z = [temp[temp.index('10')]+temp[temp.index('10') + 1], temp[temp.index('11')]+temp[temp.index('11') + 1], temp[temp.index('12')]+temp[temp.index('12') + 1], temp[temp.index('13')]+temp[temp.index('13') + 1],  temp[temp.index('14')]+temp[temp.index('14') + 1]]
+            return(z)
         elif number == list(range(min(number), max(number)+1)):
-            return('Mastirida')
-            quit()
+            z = [] 
+            lin = 0
+            while lin < 5:
+                z.append(temp[temp.index(str(min(number)))] + temp[temp.index(str(min(number))) + 1])
+                temp.pop(temp[temp.index(str(min(number))) + 1], temp[temp.index(str(min(number)))])
+                lin += 1
+            return(z)
         else:
             return('Mast')
-            quit()
-    if number[0] == number[1] and number[0] == number[2] and number[0] == number[3]:
+    elif number[0] == number[1] and number[0] == number[2] and number[0] == number[3]:
         return('Nelik')
-        quit()
-    if number[1] == number[2] and number[1] == number[3] and number[1] == number[4]:
+    elif number[1] == number[2] and number[1] == number[3] and number[1] == number[4]:
         return('Nelik')
-        quit()
-    if number[0] == number[1] and number[0] == number[2] and number[3] == number[4]:
+    elif number[0] == number[1] and number[0] == number[2] and number[3] == number[4]:
         return('Maja')
-        quit()
-    if number[0] == number[1] and number[2] == number[3] and number[2] == number[4]:
+    elif number[0] == number[1] and number[2] == number[3] and number[2] == number[4]:
         return('Maja')
-        quit()
-    if number == list(range(min(number), max(number)+1)):
+    elif number == list(range(min(number), max(number)+1)):
         return('Rida')
-        quit()
-    if number[0] == number[1] and number[0] == number[2]:
+    elif number[0] == number[1] and number[0] == number[2]:
         return('Kolmik')
-        quit()
-    if number[2] == number[3] and number[2] == number[4]:
+    elif number[2] == number[3] and number[2] == number[4]:
         return('Kolmik')
-        quit()
-    if number[0] == number[1] and number[2] == number[3] or number[0] == number[1] and number[3] == number[4]:
+    elif number[0] == number[1] and number[2] == number[3] or number[0] == number[1] and number[3] == number[4]:
         return('Kaks paari')
-        quit()
-    if number[3] == number[4] and number[0] == number[1] or number[3] == number[4] and number[1] == number[2]:
+    elif number[3] == number[4] and number[0] == number[1] or number[3] == number[4] and number[1] == number[2]:
         return('Kaks paari')
-        quit()
-    if number[0] == number[1] or number[1] == number[2] or number[2] == number[3] or number[3] == number[4]:
+    elif number[0] == number[1] or number[1] == number[2] or number[2] == number[3] or number[3] == number[4]:
         return('Üks paar')
     else:
         return('Kõrge kaart')
