1f0bfdb33f9f54dfb52119834afc14db92c6e2a4
kaaren123 <tensonkaaren@gmail.com>
2023-12-18 12:20:42 +0200

    uuendus

--- slots/slots.py
+++ slots/slots.py
@@ -14,15 +14,8 @@ class ruut:
          self.pilt = pilt
          self.height = height
          self.screen = screen
-    def drawRect(self):
-        button = pygame.Rect(self.x, self.y, self.width, self.height)
-        pygame.draw.rect(self.screen, self.varv, button, 0, 7)
-        if self.tekst != "":
-            self.drawText()
-    def drawText(self):
-        text_surf = self.font.render(self.tekst, True, (0,0,0))
-        text_rect = text_surf.get_rect(center=(self.x+self.width//2, self.y+self.height//2))
-        self.screen.blit(text_surf, text_rect)
+    def drawRect(self,):
+        self.screen.blit(self.pilt, (self.x, self.y))
 def slots(screen):
     pygame.init()
     red = (255,67,89)
@@ -35,10 +28,18 @@ def slots(screen):
     ruudud3=[]
     kiirus=0
     vajutus=False
+
+    smallfont = pygame.font.SysFont('Corbel',60)
+    text = smallfont.render('MENU' , True , white)  
+    pilt1=(pygame.transform.scale(pygame.image.load("Project-Draco-Hesperidum\kirsid.jpg").convert(), (110, 120)))
+    pilt2= (pygame.transform.scale(pygame.image.load("Project-Draco-Hesperidum\Täringud.jpg").convert(), (110, 120)))
+    pilt3=(pygame.transform.scale(pygame.image.load("Project-Draco-Hesperidum\õnn.jpg").convert(), (110, 120)))
+    pilt4=(pygame.transform.scale(pygame.image.load("Project-Draco-Hesperidum\õnn.jpg").convert(), (110, 120)))
+    pildid=[pilt1, pilt2, pilt3, pilt4]
     for i in range(4):
-        ruudud1.append(ruut(kohadx[0], kohady[i],120, 110, white, nimed[i], pygame.font.Font('freesansbold.ttf', 40), 0, screen))
-        ruudud2.append(ruut(kohadx[1], kohady[i],120, 110, white, nimed[i], pygame.font.Font('freesansbold.ttf', 40), 0, screen))
-        ruudud3.append(ruut(kohadx[2], kohady[i],120, 110, white, nimed[i], pygame.font.Font('freesansbold.ttf', 40), 0, screen))
+        ruudud1.append(ruut(kohadx[0], kohady[i],120, 110, white, nimed[i], pygame.font.Font('freesansbold.ttf', 40),pildid[i] , screen))
+        ruudud2.append(ruut(kohadx[1], kohady[i],120, 110, white, nimed[i], pygame.font.Font('freesansbold.ttf', 40),pildid[i], screen))
+        ruudud3.append(ruut(kohadx[2], kohady[i],120, 110, white, nimed[i], pygame.font.Font('freesansbold.ttf', 40), pildid[i], screen))
 
     while True:
         mouse = pygame.mouse.get_pos()  
@@ -49,8 +50,12 @@ def slots(screen):
                 if 100 <= mouse[0] <= 200 and 400 <= mouse[1] <= +460: 
                         kiirus=2
                         vajutus=True
+                if 0 <= mouse[0]<= 200 and 0 <= mouse[1] <= 100: 
+                    return("menu")
         screen.fill(red)
+        pygame.draw.rect(screen,(0,0,0),[0,0,200,100],0,0)
         pygame.draw.rect(screen,(0,0,0),[400,200,400,400], 0, 0)
+        screen.blit(text, (0,0))
         for el in ruudud1: 
             el.drawRect()
         for el in ruudud2: 
@@ -91,7 +96,7 @@ def slots(screen):
         pygame.draw.rect(screen,white,[100,400,100,60], 0, 7)
         pygame.display.update()
 
-#SCREEN_WIDTH=1100
-#SCREEN_HEIGHT=700
-#screen=pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-#slots(screen)
+SCREEN_WIDTH=1100
+SCREEN_HEIGHT=700
+screen=pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+lots(screen)
